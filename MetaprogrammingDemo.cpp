fd1d54b4443de26ba19c698b3900f5b3cb58f116
aspirin <f1193968991@126.com>
2025-11-01 22:13:23 +0800

    feat: TypeHash

--- MetaprogrammingDemo.cpp
+++ MetaprogrammingDemo.cpp
@@ -3,28 +3,29 @@
 #include "Query.h"
 #include "World.h"
 #include "TypeList.h"
+#include "TypeHash.h"
 
 void sys_no_param() {}
 
 void sys_bad_params(int i) {}
 
-void sys_bad_params2(Query<int> q) {};
+// void sys_bad_params2(Query<int> q) {};
 
 void sys_good_params(Query<std::tuple<int>>) { return; }
 
 void sys_good_params2(Query<std::tuple<float>, With<int>> q1, Query<std::tuple<int, float>> q2)
 {
-    for (auto iter: q1)
-    {
-
-    }
+    // for (auto iter: q1)
+    // {
+    //
+    // }
 }
 
 void sys1(Query<std::tuple<int, float>> q) {}
 
 int main(int argc, char* argv[])
 {
-    World world;
-    world.add_system(&sys_good_params2); // CTAD
+    constexpr auto id = TypeHash::index_of<int>();
+    std::cout << id;
     return 0;
 }
---
+++ TypeHash.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <cstdint>
+
+class TypeHash
+{
+private:
+    static constexpr uint32_t FNV_OFFSET_BASIS = 2166136261u;
+    static constexpr uint32_t FNV_PRIME = 16777619u;
+
+    template <size_t Length>
+    static consteval uint32_t hash_impl(const char (&str)[Length])
+    {
+        uint32_t hash = FNV_OFFSET_BASIS;
+        for (auto i = 0; i < Length - 1; ++i) // we skip '\n' by less than Length - 1
+        {
+            hash ^= static_cast<uint32_t>(str[i]);
+            hash *= FNV_PRIME;
+        }
+        return hash;
+    }
+
+public:
+    template <typename T>
+    static consteval uint32_t index_of()
+    {
+#if defined(_MSC_VER)
+        return hash_impl(__FUNCSIG__);
+#elif defined(__GNUC__) || defined(__clang__)
+        return hash_impl(__PRETTY_FUNCTION__);
+#elif
+        static_assert("Unsupported typename backend.");
+#endif
+    }
+};
